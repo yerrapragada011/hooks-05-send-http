e3346d03db27ce08df8117f45ef58f5e0c89ed6a
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-06-30 15:26:36 -0400

    Understanding useEffect() Dependencies

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -25,6 +25,10 @@ const Ingredients = () => {
       });
   }, []);
 
+  useEffect(() => {
+    console.log('RENDERING INGREDIENTS', userIngredients);
+  }, [userIngredients]);
+
   const addIngredientHandler = (ingredient) => {
     fetch(
       'https://react-hooks-update-e8923-default-rtdb.firebaseio.com/ingredients.json',
